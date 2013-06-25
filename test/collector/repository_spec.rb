76a71049239763f7b34a18b7f8433744558eb36f
Brandon Weiss <brandon@anti-pattern.com>
2013-06-25 13:44:23 -0700

    Fixing a bug that crept up relating to the test model name

--- test/collector/repository_spec.rb
+++ test/collector/repository_spec.rb
@@ -9,27 +9,27 @@ describe Collector::Repository do
     end
 
     Object.send(:remove_const, :TestRepository) if Object.const_defined?(:TestRepository)
-    class TestRepository
+    class TestModelRepository
       include Collector::Repository
     end
   end
 
   it "has a model derived from its class name" do
-    TestRepository.model.name.must_equal "Test"
+    TestModelRepository.model.name.must_equal "TestModel"
   end
 
   it "has a collection_name derived from its model" do
-    TestRepository.collection_name.must_equal "tests"
+    TestModelRepository.collection_name.must_equal "test_models"
   end
 
   describe "collection" do
     describe "when a connection is set" do
       it "returns the mongo collection" do
         collection = mock()
-        connection = mock { stubs(:[]).with("tests").returns(collection) }
+        connection = mock { stubs(:[]).with("test_models").returns(collection) }
         Collector.stubs(:connection).returns(connection)
 
-        TestRepository.collection.must_equal collection
+        TestModelRepository.collection.must_equal collection
       end
     end
   end
@@ -37,33 +37,32 @@ describe Collector::Repository do
   describe "save" do
     it "touches the model and then saves it" do
       model = mock(:touch)
-      TestRepository.expects(:save_without_updating_timestamps).with(model)
-      TestRepository.save(model)
+      TestModelRepository.expects(:save_without_updating_timestamps).with(model)
+      TestModelRepository.save(model)
     end
   end
 
   describe "save_without_updating_timestamps" do
     it "serializes the model and then saves it into the collection" do
       model = stub()
-      TestRepository.expects(:serialize!).with(model).returns({ foo: "bar" })
+      TestModelRepository.expects(:serialize!).with(model).returns({ foo: "bar" })
 
       collection = mock(save: { foo: "bar" })
-      TestRepository.stubs(:collection).returns(collection)
+      TestModelRepository.stubs(:collection).returns(collection)
 
-      TestRepository.save_without_updating_timestamps(model)
+      TestModelRepository.save_without_updating_timestamps(model)
     end
 
-    it "returns the model with the its id set" do
-      model = TestModel.new
-      TestRepository.expects(:serialize!).with(model).returns({ foo: "bar" })
+    it "returns the model with its id set" do
+      model = TestModel.new(id: "123abc")
+      TestModelRepository.expects(:serialize!).with(model).returns({ foo: "bar" })
 
       object_id  = stub(to_s: "123abc")
       collection = mock { expects(:save).with({ foo: "bar" }).returns(object_id) }
-      TestRepository.stubs(:collection).returns(collection)
+      TestModelRepository.stubs(:collection).returns(collection)
 
-      updated_model = TestRepository.save_without_updating_timestamps(model)
+      updated_model = TestModelRepository.save_without_updating_timestamps(model)
       updated_model.must_equal model
-      updated_model.id.must_equal "123abc"
     end
   end
 
@@ -72,43 +71,43 @@ describe Collector::Repository do
       model = stub(id: "50c58f4ab392d4381a000001")
 
       collection = mock { expects(:remove).with(_id: BSON::ObjectId("50c58f4ab392d4381a000001")) }
-      TestRepository.stubs(:collection).returns(collection)
+      TestModelRepository.stubs(:collection).returns(collection)
 
-      TestRepository.delete(model)
+      TestModelRepository.delete(model)
     end
   end
 
   describe "serialize!" do
     it "normalize id to _id and converts to a BSON::ObjectId" do
       model = mock(attributes: { id: "50c58f4ab392d4381a000001", foo: "bar" })
-      TestRepository.serialize!(model).must_equal({ "_id" => BSON::ObjectId("50c58f4ab392d4381a000001"), "foo" => "bar" })
+      TestModelRepository.serialize!(model).must_equal({ "_id" => BSON::ObjectId("50c58f4ab392d4381a000001"), "foo" => "bar" })
     end
 
     it "returns a model's attributes without nil values" do
       model = mock(attributes: { foo: "bar", nothing: nil })
-      TestRepository.serialize!(model).must_equal({ "foo" => "bar" })
+      TestModelRepository.serialize!(model).must_equal({ "foo" => "bar" })
     end
   end
 
   describe "serialize" do
     it "returns a model's attributes" do
       model = mock(attributes: { foo: "bar" })
-      TestRepository.serialize(model).must_equal({ "foo" => "bar" })
+      TestModelRepository.serialize(model).must_equal({ "foo" => "bar" })
     end
   end
 
   describe "deserialize!" do
     it "normalizes _id to id and converts to a string id" do
-      TestRepository.expects(:deserialize).with("id" => "50c58f4ab392d4381a000001", "name" => "Brandon")
-      TestRepository.deserialize!(_id: BSON::ObjectId("50c58f4ab392d4381a000001"), name: "Brandon")
+      TestModelRepository.expects(:deserialize).with("id" => "50c58f4ab392d4381a000001", "name" => "Brandon")
+      TestModelRepository.deserialize!(_id: BSON::ObjectId("50c58f4ab392d4381a000001"), name: "Brandon")
     end
   end
 
   describe "deserialize" do
     it "instantiates a new model from a hash of attributes" do
       attributes = { first_name: "Brandon", last_name: "Weiss" }
-      TestRepository.model.expects(:new).with(attributes)
-      TestRepository.deserialize(attributes)
+      TestModelRepository.model.expects(:new).with(attributes)
+      TestModelRepository.deserialize(attributes)
     end
   end
 
@@ -117,82 +116,82 @@ describe Collector::Repository do
       document_1 = stub
       document_2 = stub
       documents  = [document_1, document_2]
-      TestRepository.expects(:deserialize!).with(document_1)
-      TestRepository.expects(:deserialize!).with(document_2)
+      TestModelRepository.expects(:deserialize!).with(document_1)
+      TestModelRepository.expects(:deserialize!).with(document_2)
       collection = mock { expects(:find).with(attribute: "value").returns(documents) }
-      TestRepository.expects(:collection).returns(collection)
-      TestRepository.find_by(attribute: "value")
+      TestModelRepository.expects(:collection).returns(collection)
+      TestModelRepository.find_by(attribute: "value")
     end
 
     it "finds all documents if no attributes are given" do
       document_1 = stub
       document_2 = stub
       documents  = [document_1, document_2]
-      TestRepository.expects(:deserialize!).with(document_1)
-      TestRepository.expects(:deserialize!).with(document_2)
+      TestModelRepository.expects(:deserialize!).with(document_1)
+      TestModelRepository.expects(:deserialize!).with(document_2)
       collection = mock { expects(:find).with({}).returns(documents) }
-      TestRepository.expects(:collection).returns(collection)
-      TestRepository.find_by
+      TestModelRepository.expects(:collection).returns(collection)
+      TestModelRepository.find_by
     end
 
     it "normalizes the id into a BSON::ObjectId if it's valid ObjectId" do
       collection = mock { expects(:find).with(_id: BSON::ObjectId("50c58f4ab392d4381a000001")).returns([]) }
-      TestRepository.expects(:collection).returns(collection)
-      TestRepository.find_by(_id: "50c58f4ab392d4381a000001")
+      TestModelRepository.expects(:collection).returns(collection)
+      TestModelRepository.find_by(_id: "50c58f4ab392d4381a000001")
     end
 
     it "does nothing with the id if it's not a valid BSON::ObjectId" do
       collection = mock { expects(:find).with(_id: "lols").returns([]) }
-      TestRepository.expects(:collection).returns(collection)
-      TestRepository.find_by(_id: "lols")
+      TestModelRepository.expects(:collection).returns(collection)
+      TestModelRepository.find_by(_id: "lols")
     end
   end
 
   describe "find_first_by" do
     it "finds the first document by a hash of attributes" do
-      TestRepository.expects(:find_by).with(attribute: "value").returns(mock(:first))
-      TestRepository.find_first_by(attribute: "value")
+      TestModelRepository.expects(:find_by).with(attribute: "value").returns(mock(:first))
+      TestModelRepository.find_first_by(attribute: "value")
     end
   end
 
   describe "all" do
     it "finds by attributes without any attributes" do
-      TestRepository.expects(:find_by).with()
-      TestRepository.all
+      TestModelRepository.expects(:find_by).with()
+      TestModelRepository.all
     end
   end
 
   describe "find_by_id" do
     it "finds by id" do
-      TestRepository.expects(:find_by).with(_id: "bson-id")
-      TestRepository.find_by_id("bson-id")
+      TestModelRepository.expects(:find_by).with(_id: "bson-id")
+      TestModelRepository.find_by_id("bson-id")
     end
   end
 
   describe "find_first_by_id" do
     it "finds first by id" do
-      TestRepository.expects(:find_first_by).with(_id: "bson-id")
-      TestRepository.find_first_by_id("bson-id")
+      TestModelRepository.expects(:find_first_by).with(_id: "bson-id")
+      TestModelRepository.find_first_by_id("bson-id")
     end
   end
 
   describe "dynamic finders" do
     it "dynamically matches find_by_ finders" do
-      TestRepository.expects(:find_by).with(email: "foobar@fibroblast.com")
-      TestRepository.find_by_email("foobar@fibroblast.com")
+      TestModelRepository.expects(:find_by).with(email: "foobar@fibroblast.com")
+      TestModelRepository.find_by_email("foobar@fibroblast.com")
     end
 
     it "dynamically matches find_first_by_ finders" do
-      TestRepository.expects(:find_first_by).with(email: "foobar@fibroblast.com")
-      TestRepository.find_first_by_email("foobar@fibroblast.com")
+      TestModelRepository.expects(:find_first_by).with(email: "foobar@fibroblast.com")
+      TestModelRepository.find_first_by_email("foobar@fibroblast.com")
     end
 
     it "responds to dynamically matched find_by_ finders" do
-      TestRepository.respond_to?(:find_by_email).must_equal true
+      TestModelRepository.respond_to?(:find_by_email).must_equal true
     end
 
     it "responds to dynamically matched find_first_by_ finders" do
-      TestRepository.respond_to?(:find_first_by_email).must_equal true
+      TestModelRepository.respond_to?(:find_first_by_email).must_equal true
     end
   end
 
